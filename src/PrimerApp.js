105f7fec4efd11f44f2813f4da66b9b30ef62e18
Donovan <sjb_ramos@hotmail.com>
2020-11-03 23:14:23 -0500

    add props and propTypes

---
+++ src/PrimerApp.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+
+//FC
+const PrimeraApp = ({ saludo, subtitulo }) => {
+  // const saludo = {
+  //   nombre: "Eder",
+  //   edad: 33,
+  // };
+  // console.log(props);
+  // const saludo = "Hola Mundo";
+
+  return (
+    <>
+      <h1> {saludo} </h1>
+      {/* <pre> {JSON.stringify(saludo, null, 3)} </pre> */}
+      <p>{subtitulo}</p>
+    </>
+  );
+};
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired,
+};
+
+PrimeraApp.defaultProps = {
+  subtitulo: "Soy un subtitulo",
+};
+
+export default PrimeraApp;
